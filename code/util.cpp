1e46ff957b8f9aeb34ad9c848f8be4bd1947341d
Stefan Toman <git@stoman.de>
2018-03-01 20:09:55 +0100

    add easy scoring function

--- code/util.cpp
+++ code/util.cpp
@@ -25,13 +25,8 @@ void debug (T x)
 //util classes
 struct Input {
   int r, c, f, n, b, t;
-  //ride i = 2i->2i+1, start 2n+2, target 2n+3 -> 2n+4 nodes
-  vector<vector<int>> adj;
-  vector<vector<ll>> dist;
-  vector<vector<int>> car;
   vector<vector<int>> paths;
-  vector<ll> ra, rb, rx, ry, rs, rt;
-  vector<int> curride;
+  vector<ll> ra, rb, rx, ry, rs, rf;
 };
 
 //input handling
@@ -42,12 +37,9 @@ void readInput(Input& input, istream& in) {
   input.rx.resize(input.n);
   input.ry.resize(input.n);
   input.rs.resize(input.n);
-  input.rt.resize(input.n);
-  input.adj.resize(2*input.n+4);
-  input.dist.resize(2*input.n+4);
-  input.car.resize(2*input.n+4);
+  input.rf.resize(input.n);
   input.paths.resize(input.n);
   for(int i = 0; i < input.n; i++) {
-    in >> input.ra[i] >> input.rb[i] >> input.rx[i] >> input.ry[i] >> input.rs[i] >> input.rt[i];
+    in >> input.ra[i] >> input.rb[i] >> input.rx[i] >> input.ry[i] >> input.rs[i] >> input.rf[i];
   }
 }
