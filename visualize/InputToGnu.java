585b832d186235a607a3546d211d7a9ad336c459
Christian Müller <christian.mueller@tum.de>
2018-03-01 19:39:38 +0100

    stuff

--- visualize/InputToGnu.java
+++ visualize/InputToGnu.java
@@ -1,13 +1,30 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class InputToGnu {
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		// Sysout x, y, color
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int vehicles = sc.nextInt();
+        int n = sc.nextInt();
+        int bon = sc.nextInt();
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int s = sc.nextInt();
+            int f = sc.nextInt();
+            System.out.println(a + " " + b);
+            System.out.println(x + " " + y);
+            System.out.println();
+        }
+
+        // Sysout numbers
+    }
+
 }
--- visualize/Slicer.java
+++ visualize/Slicer.java
@@ -4,7 +4,25 @@ public class Slicer {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int vehicles = sc.nextInt();
+        int n = sc.nextInt();
+        int bon = sc.nextInt();
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int s = sc.nextInt();
+            int f = sc.nextInt();
+            System.out.println(s);
+        }
+
         // Sysout numbers
-        System.out.println("12");
     }
+
 }
